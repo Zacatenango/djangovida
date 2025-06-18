ba671bf4a0aeac95b14681aaba0e8bd073dd3d96
Acoyani <acoyani@legalplay>
2025-06-18 00:43:27 -0600

    Añadiendo archivos estáticos

--- djangovida/djangovida/settings.py
+++ djangovida/djangovida/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'inicio.apps.InicioConfig',
+    "estaticos.apps.EstaticosConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "/home/dsfargeg/programacion/django-vida/djangovida/djangovida/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
